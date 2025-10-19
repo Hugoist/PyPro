94274c7663954230899c9da54feac944a5cb797f
Hugo <ky3mut4@gmail.com>
2025-10-19 19:45:23 +0300

    update INSTALLED_APPS

--- Homework_16/test_project/settings.py
+++ Homework_16/test_project/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'test_app'
 ]
 
 MIDDLEWARE = [
